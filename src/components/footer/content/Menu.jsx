a2079b68353a7a99f8c595ee222ef2c0175677b0
Mhboolor <mhstare81@gmail.com>
2023-07-31 23:36:26 +0330

    refactor : footer menu clean code

--- src/components/footer/content/Menu.jsx
+++ src/components/footer/content/Menu.jsx
@@ -28,41 +28,22 @@ function Menu() {
     ]
     
 
-
   return (
     <div className='bg-white py-5'>
         <div className='container m-auto flex items-center justify-between flex-col gap-5 md:flex-row'>
             <div className='grid grid-cols-2 sm:grid-cols-3 flex-1 gap-4 text-center sm:text-start'>
-                <div>
-                    <ul>
-                        <li className='text-gray-9 text-md font-semibold'>{menu[0].title}</li>
-                        {
-                            menu[0].items.map(item => (
-                                <li><Link to={item.link} className='text-xs text-gray-3'>{item.name}</Link></li>
-                            ))
-                        }
-                    </ul>
-                </div>
-                <div>
-                    <ul>
-                        <li className='text-gray-9 text-md font-semibold'>{menu[1].title}</li>
-                        {
-                            menu[1].items.map(item => (
-                                <li><Link to={item.link} className='text-xs text-gray-3'>{item.name}</Link></li>
-                            ))
-                        }
-                    </ul>
-                </div>
-                <div className='col-span-2 sm:col-span-1'>
-                    <ul>
-                        <li className='text-gray-9 text-md font-semibold'>{menu[2].title}</li>
-                        {
-                            menu[2].items.map(item => (
-                                <li><Link to={item.link} className='text-xs text-gray-3'>{item.name}</Link></li>
-                            ))
-                        }
-                    </ul>
-                </div>
+                {
+                    menu.map(item => (
+                        <ul>
+                            <li className='text-gray-9 text-md'>{item.title}</li>
+                            {
+                                item.items.map(sub => (
+                                    <li><Link to={sub.link} className='text-xs text-gray-3'>{sub.name}</Link></li>
+                                ))
+                            }
+                        </ul>
+                    ))
+                }
             </div>
             <div className='flex-1'>
                 <Nemads/>
