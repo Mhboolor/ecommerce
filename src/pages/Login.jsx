5ff626cf7b8df42d73d648a309d1e8dde7a43dd7
Mhboolor <mhstare81@gmail.com>
2023-08-16 21:04:21 +0330

    feat : login test code

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,7 +1,21 @@
-import React from 'react'
+import React, { useState } from 'react'
 import BreadCrumb from '../components/BreadCrumb'
+import axios from 'axios'
 
 function Login() {
+
+  const [number , setNumber] = useState("")
+
+
+  const createUser = async () => {
+    console.log(number);
+    const res = await axios.post('http://127.0.0.1:1111/auth/get-otp' , number , { headers : {'content-type': 'application/x-www',} })
+    console.log(res);
+
+  }
+
+
+
   return (
     <div className='container m-auto px-4 sm:p-0 mb-14'>
         <BreadCrumb prev={"صفحه اصلی"} prevLink={""} here={"ورود"} hereLink={""}/>
@@ -11,10 +25,10 @@ function Login() {
               <p>کاربر گرامی، خوش آمدید.</p>
               <p>لطفا شماره موبایل خود را وارد نمایید</p>
             </div>
-            <form action="" className='flex flex-col gap-5 w-full'>
-                <input type="text" id="UserNme" placeholder='شماره تفلن خود را وارد کنید ...' className='border rounded-lg flex-1 outline-none py-2 px-4'/>
-                <button className='bg-[#dc3545] border border-[#dc3545] rounded-lg py-2 flex-1 text-white'>ادامه</button>
-            </form>
+            <div action="" className='flex flex-col gap-5 w-full'>
+                <input type="text" id="UserNme" placeholder='شماره تفلن خود را وارد کنید ...' className='border rounded-lg flex-1 outline-none py-2 px-4' value={number} onChange={e => setNumber(e.target.value)}/>
+                <button className='bg-[#dc3545] border border-[#dc3545] rounded-lg py-2 flex-1 text-white' onClick={createUser}>ادامه</button>
+            </div>
         </div>
     </div>
   )
