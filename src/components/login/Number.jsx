1d6a53f85862172a34999885670580dbe640a621
Mhboolor <mhstare81@example.com>
2023-08-18 19:28:14 +0330

    feat : test api

--- src/components/login/Number.jsx
+++ src/components/login/Number.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-function Number() {
+function Number({ number, setNumber, addUser }) {
   return (
     <div className="bg-[#fff] shadow-md rounded-2xl flex-col flex items-center justify-center text-center pt-3 pb-6 px-5 gap-4 w-full sm:w-5/6 md:w-4/6 lg:w-3/6 xl:w-2/6 m-auto">
       <p className="border-b border-[#eff0f2] py-3 text-xl w-full font-medium">
@@ -10,17 +10,23 @@ function Number() {
         <p>کاربر گرامی، خوش آمدید.</p>
         <p>لطفا شماره موبایل خود را وارد نمایید</p>
       </div>
-      <div action="" className="flex flex-col gap-5 w-full">
+      <form action="#" className="flex flex-col gap-5 w-full">
         <input
           type="text"
           id="UserNme"
           placeholder="شماره تفلن خود را وارد کنید ..."
           className="border rounded-lg flex-1 outline-none py-2 px-4"
+          value={number}
+          onChange={(e) => setNumber(e.target.value)}
         />
-        <button className="bg-[#dc3545] border border-[#dc3545] rounded-lg py-2 flex-1 text-white">
+        <button
+          className="bg-[#dc3545] border border-[#dc3545] rounded-lg py-2 flex-1 text-white"
+          type="button"
+          onClick={addUser}
+        >
           ادامه
         </button>
-      </div>
+      </form>
     </div>
   );
 }
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,3 +1,4 @@
+import { useState } from 'react'
 import BreadCrumb from '../components/BreadCrumb'
 import Number from '../components/login/Number'
 import Otp from '../components/login/Otp'
@@ -5,10 +6,26 @@ import Otp from '../components/login/Otp'
 function Login() {
 
 
+  const [number  , setNumber] = useState("")
+
+  const mobile = {mobile : number.toString()}
+  
+  const addUser = async (e) => {
+    e.preventDefault()
+    const response = await fetch("http://127.0.0.1:1111/auth/get-otp" , {
+      method : "POST",
+      "Content-Type": "application/json",
+      body : JSON.stringify(mobile)
+    });
+    const data = await response.json();
+    console.log(data);
+  }
+
+
   return (
     <div className='container m-auto px-4 sm:p-0 mb-14'>
         <BreadCrumb prev={"صفحه اصلی"} prevLink={""} here={"ورود"} hereLink={""}/>
-        <Number/>
+        <Number number={number} setNumber={setNumber} addUser={addUser}/>
         <Otp/>
     </div>
   )
