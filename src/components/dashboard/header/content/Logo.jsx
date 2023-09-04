9300de43ebdb6e7e8c41ebeefb30efd2e8e930fe
Bolori <109837376+Mhboolor@users.noreply.github.com>
2023-09-04 07:28:50 +0330

    feat : coding dashboard logo component

--- src/components/dashboard/header/content/Logo.jsx
+++ src/components/dashboard/header/content/Logo.jsx
@@ -1,9 +1,18 @@
-import React from 'react'
+import React from "react";
+import { RiMenu5Line } from "react-icons/ri";
+import { Link } from "react-router-dom";
 
 function Logo() {
   return (
-    <div>Logo</div>
-  )
+    <div className="flex items-center gap-5">
+      <button className="text-2xl bg-gray-200 p-1 rounded-md text-gray-6 duration-150 hover:bg-gray-300">
+        <RiMenu5Line/>
+      </button>
+      <Link>
+        <img src="./images/dashboard/logo.png" alt="" className=" w-36 h-7"/>
+      </Link>
+    </div>
+  );
 }
 
-export default Logo
+export default Logo;
